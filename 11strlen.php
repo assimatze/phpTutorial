f426414b395f17ed6b62316a92046a3f448f0d63
assimatze <matthias.misior@gmx.de>
2017-05-08 17:41:41 +0200

    remove spaces at the file end

--- 11strlen.php
+++ 11strlen.php
@@ -4,6 +4,4 @@ $name = "matthias  ";
 echo 'Die länge des strings $name => strlen($name): '.strlen($name);
 //Änderungen für github
 echo 'Die länge des strings $name => strlen($name): '.strlen($name);
-
-
  ?>
