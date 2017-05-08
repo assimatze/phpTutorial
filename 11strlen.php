93af4fb93674c5ed60b961a298d8f3271141b4b3
assimatze <matthias.misior@gmx.de>
2017-05-08 17:38:37 +0200

    add new echo line to see some change

--- 11strlen.php
+++ 11strlen.php
@@ -2,5 +2,7 @@
 
 $name = "matthias  ";
 echo 'Die länge des strings $name => strlen($name): '.strlen($name);
+//Änderungen für github
+echo 'Die länge des strings $name => strlen($name): '.strlen($name);
 
  ?>
